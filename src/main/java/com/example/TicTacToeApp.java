b43cfb467fc98944e3ded9366ecf520162a10fc4
chumomega <31514721+chumomega@users.noreply.github.com>
2018-07-08 12:34:06 -0500

    fixed duplicte controller

--- src/main/java/com/example/TicTacToeApp.java
+++ src/main/java/com/example/TicTacToeApp.java
@@ -22,12 +22,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import javax.sql.DataSource;
 
 import java.sql.SQLException;
 
-@Controller
 @SpringBootApplication
 public class TicTacToeApp {
   @Value("${spring.datasource.url}")
